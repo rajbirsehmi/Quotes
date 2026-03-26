b6b1e937df8557a361ae105b01e776aaf2ced868
Rajbir Sehmi <raj.sehmi1@yahoo.com>
2026-03-25 19:40:53 -0700

    Added HorizontalDivider to QuotesBySubjectScreen.kt

--- app/src/main/java/com/creative/quotes/ui/screens/QuotesBySubjectScreen.kt
+++ app/src/main/java/com/creative/quotes/ui/screens/QuotesBySubjectScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.rememberModalBottomSheetState
@@ -15,6 +16,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.creative.quotes.ui.components.AddQuoteBottomSheet
@@ -65,6 +67,11 @@ fun QuotesBySubjectScreen(
                             onQuoteClick(id)
                         }
                     }
+                )
+                HorizontalDivider(
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp),
+                    thickness = 0.5.dp
                 )
             }
         }
