b66e7c1ffcc88c4ab393da77ec5bb7b14e09b598
Rajbir Sehmi <raj.sehmi1@yahoo.com>
2026-04-29 17:11:05 -0700

    Increased the timeout

--- app/src/androidTest/java/com/creative/quotes/TestScreenOneAddQuote.kt
+++ app/src/androidTest/java/com/creative/quotes/TestScreenOneAddQuote.kt
@@ -36,7 +36,7 @@ class TestScreenOneAddQuote {
         }
     }
 
-    @FlakyTest
+
     @Test
     fun clickOnSaveQuoteButton_verifyQuoteIsAdded() {
         QuoteRobot.withRobot(composeTestRule) {
